4c79899f6ec6f7a3c79e4d54d32dae5b027a94b2
millsgerald <30513476+millsgerald@users.noreply.github.com>
2017-08-28 15:06:26 +1000

    Create loops_turtle.py

---
+++ loops_turtle.py
@@ -0,0 +1,27 @@
+import turtle
+
+#nbrsides = 5
+#for weirdname in range(nbrsides) :
+#    turtle.forward(100)
+#    turtle.right(360/nbrsides)
+#    for steps in range(nbrsides) :
+#        turtle.forward(50) 
+#        turtle.right(360/nbrsides)
+
+
+#for colour in ['red','green','blue','black'] :
+#    turtle.color(colour)
+#    turtle.forward(100)
+#    turtle.left(90)
+
+#counter = 0
+#while counter < 4:
+#    turtle.forward(100)
+#    turtle.right(90)
+#    counter += 1 # This is the exact same thing as counter = counter + 1
+
+
+answer = '0'
+while answer != '42':
+    answer = input('What is the answer to the ultimate question of life, the universe and everything? ')
+print('Congratulations - you are right!!')
